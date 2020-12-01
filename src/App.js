582d914b59975bcd57afbcfb304c5d25467d9fda
Gabriellji <gabriellji@yahoo.com>
2020-12-01 16:35:52 +0300

    feat: route components and basic componets layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
+import React, { useState } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
+import Navbar from './components/navbar';
+import Home from './components/home'
+import Logo from './components/logo';
+import Shop from './components/shop';
+import Map from './components/map';
+import Contact from './components/contact';
+
+
+const App = () => {
+
+  const [home, setHome] = useState({
+    title: 'This is the home component',
+  });
+
+  const [logo, setLogo] = useState({
+    title: 'This is logo component',
+  });
+
+  const [shop, setShop] = useState({
+    title: 'This is shop component',
+  });
+
+  const [map, setMap] = useState({
+    title: 'This is map component',
+  });
+
+  const [contact, setContact] = useState({
+    title: 'This is contact component',
+  });
+
+
   return (
-    <div className="App">
-      <h1>Hello there!</h1>
-    </div>
-  );
-}
+    <>
+    <Navbar />
+    <Switch>
+      <Route
+        exact path='/'
+        render={props =>
+          <Home {...props} title={home.title} />
+        }
+      />
+      <Route path='/logo'
+        render={props =>
+          <Logo {...props} title={logo.title} />
+        }
+      />
+      <Route path='/shop'
+        render={props =>
+          <Shop {...props} title={shop.title} />
+        }
+      />
+      <Route path='/map'
+        render={props =>
+          <Map {...props} title={map.title} />
+        }
+      />
+      <Route path='/contact'
+        render={props =>
+          <Contact {...props} title={contact.title} />
+        }
+      />
+    </Switch>
+    </>
+  )
+};
 
 export default App;
---
+++ src/components/contact/Contact.js
@@ -0,0 +1,7 @@
+const Contact = props => (
+    <div>
+        <h1>{props.title}</h1>
+    </div>
+)
+
+export default Contact;
