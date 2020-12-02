dffd6c8f92c7d662924d9fdfe72c2e586963bda2
Teiko <teikomancini1993@gmail.com>
2020-12-02 18:52:28 +0100

    first api call done. need to work on the second api call for pictures

--- src/context/APIContext.js
+++ src/context/APIContext.js
@@ -1,35 +1,46 @@
 import { render } from "@testing-library/react";
-import React, {Component} from "react";
-export const MyAPIContext = React.createContext()
+import React, { Component } from "react";
+export const MyAPIContext = React.createContext();
 
-const APIkey=process.env.REACT_APP_EBAY_KEY
+const APIkey = process.env.REACT_APP_EBAY_KEY;
 class MyAPIProvider extends Component {
 
-    state= {
-        data: [],
-    }
-    
-componentDidMount(){
-    const cors = `https://cors-anywhere.herokuapp.com/`
-    const url=`${cors}https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByCategory&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${APIkey}&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&categoryId=10181&paginationInput.entriesPerPage=12`
-    fetch(url)
-    .then(response => response.json())
-    .then(data => data.findItemsByCategoryResponse[0].searchResult[0].item)
-    .then(result => this.setState({data: result}))
-}
+state = {
+    items: [],
+  };
 
+  componentDidMount() {     //keep this itemsNumber low while in production
+    const itemsNumber = 3;  //how many items to pull. note that the api calls are 3x(n+1) (e.g. with 3 items i do 12api calls).
+    let category = [
+      { id: 181033, data: [] },   //set the category. GARDENING
+      { id: 15032, data: [] },    //set the category. PHONES
+      { id: 63861, data: [] },    //set the category. CLOTHES
+    ];
+    const cors = `https://cors-anywhere.herokuapp.com/`;   //anti CORS <3
+    const apiCall = (category) => {
+      const url = `${cors}https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByCategory&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${APIkey}&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&categoryId=${category.id}&paginationInput.entriesPerPage=${itemsNumber}`;
+      fetch(url)       //the main api call function
+        .then((response) => response.json())
+        .then(
+          (data) => data.findItemsByCategoryResponse[0].searchResult[0].item
+        )
+        .then((result) => (category.data = result));
+      console.log(category);   //check. to remove later
+    };
+    category.map((e) => apiCall(e));  //making 3 api calls to retrieve data from 3 categories
+  }
 
-
-
-render() {
+  render() {
     return (
-        <MyAPIContext.Provider value={{
-            state: this.state,
-        }}>
-            {this.props.children}
-        </MyAPIContext.Provider>
-    )
-}
+      <MyAPIContext.Provider
+        value={{
+          state: this.state,
+        }}
+      >
+        {this.props.children}
+      </MyAPIContext.Provider>
+    );
+  }
 }
 
-export default MyAPIProvider
+export default MyAPIProvider;
