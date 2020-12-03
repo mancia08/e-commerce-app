8a5b57a59ead11689e16710e57d5e8622dd125e8
mancia08 <teikomancini1993@gmail.com>
2020-12-03 22:37:10 +0100

    working

--- src/context/APIContext.js
+++ src/context/APIContext.js
@@ -4,30 +4,47 @@ export const MyAPIContext = React.createContext();
 
 const APIkey = process.env.REACT_APP_EBAY_KEY;
 class MyAPIProvider extends Component {
-
-state = {
+  state = {
     items: [],
   };
 
-  componentDidMount() {     //keep this itemsNumber low while in production
-    const itemsNumber = 3;  //how many items to pull. note that the api calls are 3x(n+1) (e.g. with 3 items i do 12api calls).
+  componentDidMount() {
+    //keep this itemsNumber low while in production
+    const itemsNumber = 3; //how many items to pull. note that the api calls are 3x(n+1) (e.g. with 3 items i do 12api calls).
+    //note that if i want 3 items per store i need to pull 9 items
     let category = [
-      { id: 181033, data: [] },   //set the category. GARDENING
-      { id: 15032, data: [] },    //set the category. PHONES
-      { id: 63861, data: [] },    //set the category. CLOTHES
+      { id: 181033, data: [] }, //set the category. GARDENING
+      { id: 15032, data: [] }, //set the category. PHONES
+      { id: 63861, data: [] }, //set the category. CLOTHES
     ];
-    const cors = `https://cors-anywhere.herokuapp.com/`;   //anti CORS <3
+    let items = [];
+    const cors = `https://cors-anywhere.herokuapp.com/`; //anti CORS <3
     const apiCall = (category) => {
-      const url = `${cors}https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByCategory&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${APIkey}&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&categoryId=${category.id}&paginationInput.entriesPerPage=${itemsNumber}`;
-      fetch(url)       //the main api call function
+      const categoryUrl = `${cors}https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByCategory&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${APIkey}&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&categoryId=${category.id}&paginationInput.entriesPerPage=${itemsNumber}`;
+      const itemUrl = `${cors}https://open.api.ebay.com/shopping?callname=GetSingleItem&responseencoding=JSON&appid=${APIkey}&siteid=0&version=967&ItemID=`;
+      fetch(categoryUrl) //the main api call function
         .then((response) => response.json())
         .then(
-          (data) => data.findItemsByCategoryResponse[0].searchResult[0].item
+          (itemData) =>
+            itemData.findItemsByCategoryResponse[0].searchResult[0].item
         )
-        .then((result) => (category.data = result));
-      console.log(category);   //check. to remove later
+        .then((data) => {
+          //start of second api call
+          const dataPromisesArray = data.map((el) =>
+            fetch(`${itemUrl}${el.itemId}`).then((response) => response.json())
+          );
+          return Promise.all(dataPromisesArray);
+        })
+        .then((finalData) => {
+          category.data = finalData;
+        });
     };
-    category.map((e) => apiCall(e));  //making 3 api calls to retrieve data from 3 categories
+    category.map((e) => apiCall(e)); //making 3 api calls to retrieve data from 3 categories
+    console.log(category);
+    category.map((categories) =>
+      items.push({ items: categories.data.map((item) => item.Item) })
+    );
+    console.log(items);
   }
 
   render() {
