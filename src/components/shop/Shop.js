aa1beeb1b83080f5d3bcda86b34560de0d4cee7f
Gabriellji <gabriellji@yahoo.com>
2020-12-04 01:01:52 +0300

    hifeat: implement router system in shop component

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,3 +1,4 @@
+import { Switch, Route } from 'react-router-dom'
 import ShopBanner from './shop-banner';
 
 import jewerly from './shop-banner/jewelry.png'
@@ -5,28 +6,46 @@ import jacket from './shop-banner/jacket.png'
 import laptop from './shop-banner/laptop.png'
 import ShopIconWrap from './shop-icon-wrap';
 
+import './Shop.css'
+import First from './remote-shop/first';
+import Second from './remote-shop/second';
+
 const Shop = props => (
-    <div>
-        <h1>{props.title}</h1>
-        <ShopBanner
-        title="LOCAL JEWELRY SHOPS"
-        text="Lorem lorem lorem lorem lorem lorem"
-        image={jewerly}
-        />
-        <ShopIconWrap/>
-        <ShopBanner
-        title="LOCAL CLOTHES SHOPS"
-        text="Lorem lorem lorem lorem lorem lorem"
-        image={jacket}
-        />
-        <ShopIconWrap/>
-        <ShopBanner
-        title="LOCAL ELECTRONIC SHOPS"
-        text="Lorem lorem lorem lorem lorem lorem"
-        image={laptop}
-        />
-        <ShopIconWrap/>
-    </div>
+    <Switch>
+        <Route
+            exact path='/shop'>
+            <div className="shop_container">
+                <h1>{props.title}</h1>
+                <ShopBanner
+                    title="LOCAL JEWELRY SHOPS"
+                    text="Lorem lorem lorem lorem lorem lorem"
+                    image={jewerly}
+                />
+                <ShopIconWrap />
+                <ShopBanner
+                    title="LOCAL CLOTHES SHOPS"
+                    text="Lorem lorem lorem lorem lorem lorem"
+                    image={jacket}
+                />
+                <ShopIconWrap />
+                <ShopBanner
+                    title="LOCAL ELECTRONIC SHOPS"
+                    text="Lorem lorem lorem lorem lorem lorem"
+                    image={laptop}
+                />
+                <ShopIconWrap />
+            </div>
+        </Route>
+
+        <Route path='/shop/0'>
+            <First/>
+        </Route>
+
+        <Route path='/shop/1'>
+            <Second/>
+        </Route>
+    </Switch>
+
 )
 
 export default Shop;
