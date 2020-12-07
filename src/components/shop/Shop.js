54c7d17a1104da1b119e44c299cc27b17ab0e855
mancia08 <teikomancini1993@gmail.com>
2020-12-07 12:12:24 +0100

    ok

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,7 +1,28 @@
-const Fourth = props => (
+import ShopBanner from './shop-banner';
+
+import jewerly from './shop-banner/jewelry.png'
+import jacket from './shop-banner/jacket.png'
+import laptop from './shop-banner/laptop.png'
+
+const Shop = props => (
     <div>
         <h1>{props.title}</h1>
+        <ShopBanner
+        title="LOCAL JEWELRY SHOPS"
+        text="Lorem lorem lorem lorem lorem lorem"
+        image={jewerly}
+        />
+        <ShopBanner
+        title="LOCAL CLOTHES SHOPS"
+        text="Lorem lorem lorem lorem lorem lorem"
+        image={jacket}
+        />
+        <ShopBanner
+        title="LOCAL ELECTRONIC SHOPS"
+        text="Lorem lorem lorem lorem lorem lorem"
+        image={laptop}
+        />
     </div>
 )
 
-export default Fourth;
+export default Shop;
