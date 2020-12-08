7e391d0740d17d4ce6f3668b5ea05aa552b3f9fd
Gabriellji <gabriellji@yahoo.com>
2020-12-08 19:19:56 +0300

    feat: markers layer to map

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import mapboxgl from 'mapbox-gl';
 
 import Marker from './marker';
+import Popup from './popup'
 import fetchFakeData from './api/fetchFakeData';
 
 import './Map.css';
@@ -11,55 +12,107 @@ mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
 const Map = () => {
     const mapContainerRef = useRef(null);
+    const popUpRef = useRef(new mapboxgl.Popup({ offset: 15 }));
 
     useEffect(() => {
         const map = new mapboxgl.Map({
-          container: mapContainerRef.current,
-          style: 'mapbox://styles/mapbox/streets-v11',
-          center: [-104.9876, 39.7405],
-          zoom: 12.5,
+            container: mapContainerRef.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [-104.9876, 39.7405],
+            zoom: 12.5,
         });
 
         map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
 
-        map.on('load', () => {
-            // add the data source for new a feature collection with no features
-            map.addSource('random-points-data', {
-              type: 'geojson',
-              data: {
-                type: 'FeatureCollection',
-                features: [],
-              },
-            });
-            // now add the layer, and reference the data source above by name
-            map.addLayer({
-              id: 'random-points-layer',
-              source: 'random-points-data',
-              type: 'symbol',
-              layout: {
-                // full list of icons here: https://labs.mapbox.com/maki-icons
-                'icon-image': 'bakery-15', // this will put little croissants on our map
-                'icon-padding': 0,
-                'icon-allow-overlap': true,
-              },
-            });
-          });
+        map.on('load', function () {
+            // Add an image to use as a custom marker
+            map.loadImage(
+                'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
+                function (error, image) {
+                    if (error) throw error;
+                    map.addImage('custom-marker', image);
+                    // Add a GeoJSON source with 2 points
+                    map.addSource('random-points-data', {
+                        'type': 'geojson',
+                        'data': {
+                            'type': 'FeatureCollection',
+                            'features': [
+                                {
+                                    // feature for Mapbox DC
+                                    'type': 'Feature',
+                                    'geometry': {
+                                        'type': 'Point',
+                                        'coordinates': [
+                                            -77.03238901390978,
+                                            38.913188059745586
+                                        ]
+                                    },
+                                    'properties': {
+                                        'title': 'Hey Teiko!'
+                                    }
+                                },
+                                {
+                                    // feature for Mapbox SF
+                                    'type': 'Feature',
+                                    'geometry': {
+                                        'type': 'Point',
+                                        'coordinates': [-122.414, 37.776]
+                                    },
+                                    'properties': {
+                                        'title': 'Llala Zheniya'
+                                    }
+                                }
+                            ]
+                        }
+                    });
 
-          map.on('moveend', async () => {
-            // get new center coordinates
-            const { lng, lat } = map.getCenter();
-            // fetch new data
-            const results = await fetchFakeData(lng, lat);
-            // update "random-points-data" source with new data
-            // all layers that consume the "random-points-data" data source will be updated automatically
-            map.getSource('random-points-data').setData(results);
-          });
-    
+                    // Add a symbol layer
+                    map.addLayer({
+                        'id': 'random-points-layer',
+                        'type': 'symbol',
+                        'source': 'random-points-data',
+                        'layout': {
+                            'icon-image': 'custom-marker',
+                            // get the title name from the source's "title" property
+                            'text-field': ['get', 'title'],
+                            'text-font': [
+                                'Open Sans Semibold',
+                                'Arial Unicode MS Bold'
+                            ],
+                            'text-offset': [0, 1.25],
+                            'text-anchor': 'top'
+                        }
+                    });
+                }
+            );
+        });
+
+        // map.on('moveend', async () => {
+        //     // get new center coordinates
+        //     const { lng, lat } = map.getCenter();
+        //     // fetch new data
+        //     const results = await fetchFakeData(lng, lat);
+        //     // update "random-points-data" source with new data
+        //     // all layers that consume the "random-points-data" data source will be updated automatically
+        //     map.getSource('random-points-data').setData(results);
+        // });
+
+        map.on('click', 'random-points-layer', e => {
+            if (e.features.length) {
+                const feature = e.features[0];
+                // create popup node
+                const popupNode = document.createElement('div');
+                ReactDOM.render(<Popup feature={feature} />, popupNode);
+                // set popup on map
+                popUpRef.current.setLngLat(feature.geometry.coordinates).setDOMContent(popupNode).addTo(map);
+            }
+        });
+
         return () => map.remove();
-      }, []);
+    }, []);
 
 
-      return <div className="map-container" ref={mapContainerRef} />;
+    return <div className="map-container" ref={mapContainerRef} />;
 }
 
 export default Map;
