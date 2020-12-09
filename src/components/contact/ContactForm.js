1e1db7db4e270d182eb5063a66fd0d57700cb7f4
JacopoLuri <geomjacopoluri@gmail.com>
2020-12-09 18:25:22 +0100

    contact components refractored with styled components

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,25 +1,26 @@
-// const { default: ButtonS } = require("../atoms/button/ButtonS");
+import Button from "../atoms/button/Button";
+import Text from "../atoms/text/Text";
 
 const ContactForm = props => (
-                <form>
-                <label>
-                    {props.name}:
-                    <input type="text" name="name" />
-                </label>
-                <label>
-                    {props.id}:
-                    <input type="text" name="orderID" />
-                </label>
-                <label>
-                    email:
-                    <input type="text" name="email" />
-                </label>
-                <label>
-                    Telephone number:
-                    <input type="text" name="telephoneNumber" />
-                </label>
-                <button text="Submit" onClick={props.action}>Submit</button>
-            </form>
+    <form>
+        <label>
+            <Text color="dark" size="M" text={`${props.name}:`}/>
+            <input type="text" name="name" />
+        </label>
+        <label>
+            <Text color="dark" size="M" text={`${props.id}:`} />
+            <input type="text" name="orderID" />
+        </label>
+        <label>
+            <Text color="dark" size="M" text="Email" />
+            <input type="text" name="email" />
+        </label>
+        <label>
+            <Text color="dark" size="M" text="Telephone number" />
+            <input type="text" name="telephoneNumber" />
+        </label>
+        <Button action={props.action} size="M" text="Submit" color="light" />
+    </form>
 )
 
 export default ContactForm;
