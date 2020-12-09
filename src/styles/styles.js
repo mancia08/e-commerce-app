c6b691a8def0ec77a20b579eaa386dfa8223193b
JacopoLuri <geomjacopoluri@gmail.com>
2020-12-09 10:09:20 +0100

    navbar css done for desktop

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -4,16 +4,25 @@ import styled from 'styled-components';
 export const StyledNav = styled.div`
     display: flex;
     justify-content: flex-end;
+    position: sticky;
+    top: 0;
+    background-color: var(--light);
 `;
 
 export const StyledLink = styled(NavLink)`
   color: var(--dark);
   box-sizing: border-box;
-  margin: 0;
-  padding: 0;
   font-family: var(--font-common), var(--font-fallback);
+  width: 10%;
+  text-align: center;
+  line-height: var(--height50-nav);
+  text-decoration: none;
   &.active {
     color: var(--primary);
+  }
+  :hover {
+    color: var(--light);
+    background-color: var(--primary);
   }
 `;
 
@@ -21,5 +30,9 @@ export const LogoLink = styled(NavLink)`
   margin-right: auto;
 `;
 
+export const LogoImg = styled.img`
+  width: auto;
+  height: var(--height50-nav);
+`;
 
 
