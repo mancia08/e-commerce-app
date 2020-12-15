5a3a33ce9b490cd5032ce6518fd9cbb53401ac79
mancia08 <teikomancini1993@gmail.com>
2020-12-15 14:13:20 +0100

    fix hover navbar links

--- src/components/atoms/navbar/HoverMenuContent.js
+++ src/components/atoms/navbar/HoverMenuContent.js
@@ -3,16 +3,22 @@ import { Link } from "react-router-dom";
 import { shopData } from "./../../../data/shopData";
 import Text from "./../text/Text";
 
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
+import styled from "styled-components";
 
 const HoverMenuContent = (props) => (
   <>
     {shopData.map((category, categoryIndex) => (
       <div key={categoryIndex}>
         <Text color="light" size="M" text={category.category} />
-        <div key={uuidv4()}>
+        <div key={categoryIndex} /* key={uuidv4()} */>
           {category.shops.map((shop, shopIndex) => (
-            <Link key={uuidv4()} to={`/shop/category${categoryIndex + 1}/${shopIndex}`} onClick={props.action}>
+            <Link
+            key={shopIndex}
+             /*  key={uuidv4()} */
+              to={`/shop/category${categoryIndex + 1}/${shopIndex}`}
+              onClick={props.action}
+            >
               <Text color="dark" size="S" key={shopIndex} text={shop.name} />
             </Link>
           ))}
