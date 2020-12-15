fa67dbfbfb8d5fbf2490edde57931b16c8a8d445
JacopoLuri <geomjacopoluri@gmail.com>
2020-12-15 18:46:57 +0100

    pull

---
+++ src/components/pages/map/MapImage.js
@@ -0,0 +1,23 @@
+import { theme } from '../../../data/theme';
+import styled from "styled-components";
+
+import mapimg from '../../../styles/images/mapimg.jpg';
+
+const StyledMapImage = styled.img`
+    width: 100%;
+    height: 600px;
+    object-fit: cover;
+    opacity: 0.8;
+    @media (max-width: ${theme.viewport.tablet}) {
+        height: 400px;
+    }
+`;
+
+const MapImage = () => (
+    <StyledMapImage
+        src={mapimg}
+        alt="View from a London bridge"
+    />
+)
+
+export default MapImage;
