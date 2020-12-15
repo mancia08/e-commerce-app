45330e9d58fe35924eadad1ce6adcf79dab3f74f
mancia08 <teikomancini1993@gmail.com>
2020-12-15 12:43:19 +0100

    fix homepage bug

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -16,25 +16,27 @@ const HomeModal = styled.div`
   position: absolute;
   top: 218px;
   padding-left: calc(3 * ${theme.spacer});
-  display: flex;
+  display: ${(p) => (p.isLoginClicked ? "none" : "flex")};
   flex-direction: column;
   justify-content: center;
-  @media (min-width: ${theme.viewport.tablet}){
+  @media (min-width: ${theme.viewport.tablet}) {
     width: 400px;
     left: calc(4 * ${theme.spacer});
-    text-align:center
+    text-align: center;
+    display: flex;
   }
 `;
 const ButtonsWrap = styled.div`
-  display: flex;
+  display: ${(p) => (p.isLoginClicked ? "none" : "flex")};
   justify-content: space-around;
   height: calc(3 * ${theme.sizes.buttons.L} + 4 * ${theme.spacer});
   width: 100vw;
   flex-direction: column;
   align-items: center;
-  @media (min-width: ${theme.viewport.tablet}){
+  @media (min-width: ${theme.viewport.tablet}) {
     height: calc(100vh - 400px);
-    flex-direction:row
+    flex-direction: row;
+    display: flex;
   }
 `;
 
@@ -43,23 +45,23 @@ const Home = () => (
     {(value) => (
       <main>
         <Banner />
-        <SuperNav type="home" textColor="light"/>
-       {!value.state.loginIconClicked && <> <HomeModal>
+        <SuperNav type="home" textColor="light" />
+        <HomeModal isLoginClicked={value.state.loginIconClicked}>
           <Text color="light" size="XL" text={textData.homepage.title1} />
           <Text color="light" size="S" text={textData.homepage.title2} />
           <Text color="light" size="S" text={textData.homepage.title3} />
         </HomeModal>
-        <ButtonsWrap>
-        <Link to="./shop">
-          <Button size="L" text="SHOP" color="primary" />
-        </Link>
-        <Link to="./map">
-          <Button size="L" text="MAP" color="primary" />
-        </Link>
-        <Link to="./contact">
-          <Button size="L" text="CONTACT" color="primary" />
-        </Link>
-      </ButtonsWrap></>}
+        <ButtonsWrap isLoginClicked={value.state.loginIconClicked}>
+          <Link to="./shop">
+            <Button size="L" text="SHOP" color="primary" />
+          </Link>
+          <Link to="./map">
+            <Button size="L" text="MAP" color="primary" />
+          </Link>
+          <Link to="./contact">
+            <Button size="L" text="CONTACT" color="primary" />
+          </Link>
+        </ButtonsWrap>
       </main>
     )}
   </ShopContext.Consumer>
