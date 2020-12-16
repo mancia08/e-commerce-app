c4d2bade8450fe0b55911f6e5ede520349b07cab
mancia08 <teikomancini1993@gmail.com>
2020-12-16 15:15:16 +0100

    fix: navbar done

--- src/components/atoms/navbar/HoverMenu.js
+++ src/components/atoms/navbar/HoverMenu.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { SubMenu } from "./../../../styles/styles";
 import HoverMenuContent from "./HoverMenuContent";
+import styled from "styled-components"
+import {theme} from "../../../data/theme"
 
 const HoverMenu = (props) => (
   <SubMenu onMouseOver={props.over} onMouseOut={props.out}>
