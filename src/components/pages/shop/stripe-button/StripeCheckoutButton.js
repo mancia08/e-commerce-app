ef448ded17bdb6f06664db0e8476d63b6371501e
JacopoLuri <geomjacopoluri@gmail.com>
2020-12-23 14:06:49 +0100

    product page done, modal done

--- src/components/pages/shop/stripe-button/StripeCheckoutButton.js
+++ src/components/pages/shop/stripe-button/StripeCheckoutButton.js
@@ -1,45 +1,45 @@
-import React, { useContext } from 'react';
-import StripeCheckout from 'react-stripe-checkout';
-import { MyContext } from '../../../../context/APIContext';
-import {textData} from '../../../../data/textData'
+import React, { useContext } from "react";
+import StripeCheckout from "react-stripe-checkout";
+import { MyContext } from "../../../../context/APIContext";
+import { textData } from "../../../../data/textData";
 
 import Button from "../../../subatoms/button/Button";
 
 const StripeCheckoutButton = ({ price }) => {
+  const context = useContext(MyContext);
 
-    const context = useContext(MyContext);
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51HyOJsGQ9zu5MlObNnBhMMgC5EQpWd8sCE0mZGmeoOL4J9teVtSFrLWYuRJOCA2C75iYqQJaOuv4Fjp2ymiXGpWZ00oZn7L6BB";
 
-    const priceForStripe = price * 100;
-    const publishableKey = 'pk_test_51HyOJsGQ9zu5MlObNnBhMMgC5EQpWd8sCE0mZGmeoOL4J9teVtSFrLWYuRJOCA2C75iYqQJaOuv4Fjp2ymiXGpWZ00oZn7L6BB';
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Succesful!");
+    context.setCart("");
+  };
 
-    const onToken = token => {
-        console.log(token);
-        alert('Payment Succesful!');
-        context.setCart('');
-    };
-
-    return (
-      <StripeCheckout
-        label={textData.shop.checkout.label}
-        name={textData.shop.checkout.name}
-        billingAddress
-        shippingAddress
-        currency={textData.shop.checkout.currency}
-        image="https://i.imgur.com/VCZz0Xe.png"
-        description={`${textData.shop.checkout.text}${price}`}
-        amount={priceForStripe}
-        panelLabel={textData.shop.checkout.pay}
-        token={onToken}
-        stripeKey={publishableKey}
-      >
-        <Button
-          size="M"
-          text={textData.shop.checkout.label}
-          color="primary"
-          width="parent"
-        />
-      </StripeCheckout>
-    );
-}
+  return (
+    <StripeCheckout
+      label={textData.shop.checkout.label}
+      name={textData.shop.checkout.name}
+      billingAddress
+      shippingAddress
+      currency={textData.shop.checkout.currency}
+      image="https://i.imgur.com/VCZz0Xe.png"
+      description={`${textData.shop.checkout.text}${price}`}
+      amount={priceForStripe}
+      panelLabel={textData.shop.checkout.pay}
+      token={onToken}
+      stripeKey={publishableKey}
+    >
+      <Button
+        size="M"
+        text={textData.shop.checkout.label}
+        color="primary"
+        width="parent"
+      />
+    </StripeCheckout>
+  );
+};
 
 export default StripeCheckoutButton;
