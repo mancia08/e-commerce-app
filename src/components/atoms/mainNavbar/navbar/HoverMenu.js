6827935983ec48b0b4e6cff19e411a3530e1471a
mancia08 <teikomancini1993@gmail.com>
2020-12-26 11:49:12 +0100

    removed styles.css and styes.js

--- src/components/atoms/mainNavbar/navbar/HoverMenu.js
+++ src/components/atoms/mainNavbar/navbar/HoverMenu.js
@@ -1,6 +1,27 @@
 import React from "react";
-import { SubMenu } from "../../../../styles/styles";
 import HoverMenuContent from "./HoverMenuContent";
+import styled from 'styled-components'
+import {theme} from '../../../../data/theme'
+
+export const SubMenu = styled.div`
+  display: none;
+  background-color: ${theme.colors.primary};
+  width: 80vw;
+  height: calc(${theme.spacer} * 35);
+  z-index: 1;
+  position: absolute;
+  right: calc(2 * ${theme.sizes.buttons.S});
+  top: calc(2 * ${theme.sizes.buttons.S});
+  :hover {
+    display: block;
+    display: flex;
+    justify-content: space-around;
+    padding: calc(6 * ${theme.spacer});
+  }
+  @media (max-width: 768px) {
+    display: none !important;
+  }
+`;
 
 const HoverMenu = ({over, out, action}) => (
   <SubMenu {...[over,out]}onMouseOver={over} onMouseOut={out}>
