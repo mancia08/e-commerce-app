bcc1ec3d06e9a70cfcfe96cc8d3eaa313f54ce98
mancia08 <teikomancini1993@gmail.com>
2021-01-06 12:44:15 +0100

    fix: bug fixed

--- src/_helpers/error.handler.js
+++ src/_helpers/error.handler.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { textData } from "../data/textData";
 import useSound from "use-sound";
 import error from "../data/images/error.mp3";
+import bug from "../data/images/bug.mp3";
 import styled from "styled-components";
 import { theme } from "../data/theme";
 import { StyledErrorService } from "./error.popup";
 import { StyledErrorPopUp } from "./error.interceptors";
+import { ShopContext } from "../context/ShopContext";
 
 const StyledErrorHandler = styled.p`
   color: ${theme.colors.dark};
@@ -16,7 +18,8 @@ const StyledErrorHandler = styled.p`
   z-index: 102;
 
   :hover {
-    ~ ${StyledErrorService} { /* needs to style StyledErrorService */
+    ~ ${StyledErrorService} {
+      /* needs to style StyledErrorService */
       display: block;
       width: 25vw;
       height: 25vw;
@@ -31,13 +34,16 @@ const StyledErrorHandler = styled.p`
 `;
 const ErrorHandler = () => {
   const [errorHandling, { stop }] = useSound(error, { volume: 0.5 });
+  const [bugHandling] = useSound(bug, { volume: 2.5 });
   const [accessDenied, accessError] = React.useState(true);
+  const context = useContext(ShopContext);
 
   return (
     <StyledErrorHandler
       onMouseEnter={() => {
         accessError(false);
         errorHandling();
+        context.state.isLoggedIn && bugHandling();
       }}
       onMouseLeave={() => {
         accessError(true);
