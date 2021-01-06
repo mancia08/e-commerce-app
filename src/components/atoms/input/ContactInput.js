c3d337e44ca69b1b92d143eddfebb02518f2d2a7
Gabriellji <gabriellji@yahoo.com>
2021-01-06 16:46:14 +0300

    style: fix text in btn for mobile resolution

--- src/components/atoms/input/ContactInput.js
+++ src/components/atoms/input/ContactInput.js
@@ -2,9 +2,9 @@ import { theme } from "../../../data/theme";
 import styled from "styled-components";
 
 const StyledContactInput = styled.input`
-  height: ${theme.sizes.buttons.S};
+  height: ${theme.sizes.buttons.M};
   width: 50%;
-  padding: 0 ${theme.spacer} 0 ${theme.spacer};
+  padding: ${theme.spacer};
   margin-top: ${theme.spacer};
   border: 1px solid ${theme.colors.dark};
   border-radius: calc(0.5 * ${theme.spacer});
