9c25f2e7b43099bafe1c7677be39c5cfe178dc89
mancia08 <teikomancini1993@gmail.com>
2021-01-07 11:22:20 +0100

    fix: hidden unneeded info when cart is empty

--- src/components/pages/shop/cart/Cart.js
+++ src/components/pages/shop/cart/Cart.js
@@ -62,7 +62,6 @@ const StyledCartLastSection = styled.div`
 `;
 
 const Cart = ({ textColor, type }) => {
-
   const shopContext = useContext(ShopContext);
 
   if (shopContext.payment) {
@@ -176,14 +175,16 @@ const Cart = ({ textColor, type }) => {
             text={textData.shop.cart.exit}
             width="parent"
           />
-          {shopContext.cart.length>0 && <Button
-            size="M"
-            color="primary"
-            text={textData.shop.cart.clear}
-            action={clearAllItems}
-            width="parent"
-          />}
-          <div>
+          {shopContext.cart.length > 0 && (
+            <>
+            <Button
+              size="M"
+              color="primary"
+              text={textData.shop.cart.clear}
+              action={clearAllItems}
+              width="parent"
+            />
+            <div>
             <Text size="M" color="dark" text={textData.shop.cart.items} />
             <Text size="M" color="primary" text={shopContext.cart.length} />
           </div>
@@ -191,19 +192,23 @@ const Cart = ({ textColor, type }) => {
             <Text size="M" color="dark" text={textData.shop.cart.price} />
             <Text size="M" color="primary" text={`${getTotalPrice()} £`} />
           </div>
+          </>
+          )}
         </StyledCartLastSection>
         {shopContext.state.isLoggedIn ? (
           <StripeCheckoutButton price={getTotalPrice()} />
         ) : (
-          <StyledCartLastSection>
-          <Button
-            width="parent"
-            size="M"
-            color="primary"
-            text={textData.shop.checkout.notLogged}
-            action={()=>{shopContext.loginIconToggle()}}
-          />
-         </StyledCartLastSection>
+          shopContext.cart.length > 0 && (
+            <Button
+              width="parent"
+              size="M"
+              color="primary"
+              text={textData.shop.checkout.notLogged}
+              action={() => {
+                shopContext.loginIconToggle();
+              }}
+            />
+          )
         )}
       </Modal>
     </>
