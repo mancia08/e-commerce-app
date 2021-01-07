4257f2b5621fb82e57dad78544800df1b1a0a6e5
mancia08 <teikomancini1993@gmail.com>
2021-01-07 15:10:31 +0100

    fix: api call

--- src/context/APIContext.js
+++ src/context/APIContext.js
@@ -1,112 +1,61 @@
-import React, { useState, useEffect } from 'react';
-import { items } from '../data/items';
+import React, { useState, useEffect } from "react";
+import { items } from "../data/items";
 
 const APIkey = process.env.REACT_APP_EBAY_KEY;
 
 export const MyContext = React.createContext();
 
-const MyAPIProvider = ({children}) => {
-
+const MyAPIProvider = ({ children }) => {
   const [state, setState] = useState({});
 
   const [loading, setLoading] = useState(true);
 
-  const [itemsEbay, setItems] = useState([]);
-
-  const [jewerly, setJ] = useState({id: 0, shops:[]});
-  const [clothes, setC] = useState({id: 0, shops:[]});
-  const [electr, setE] = useState({id: 0, shops:[]});
+  const [itemsEbay, setItems] = useState([
+    { id: 0, shops: [] },
+    { id: 0, shops: [] },
+    { id: 0, shops: [] },
+  ]);
 
   useEffect(() => {
     setState(items);
     getDataFromEbay();
-    setLoading(false);
-  }, [])
+  }, []);
 
   const getDataFromEbay = () => {
-    const itemsPerShop = 1; //keep this number = 1 during production. NOTE we do 3*(3n+1) API calls
-    let category = [ 
-      { id: 10968, data: [] }, //JEWELRY 
-      { id: 175759, data: [] }, //CLOTHES
-      { id: 32852, data: [] }, //ELECTRONICS
-
-    ];
-    let finalState = []
-    const apiCall = (category) => {
-      setLoading(true)
-      const categoryUrl = `https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByCategory&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${APIkey}&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&categoryId=${category.id}&paginationInput.entriesPerPage=${itemsPerShop * 3}`;
-      const itemUrl = `https://open.api.ebay.com/shopping?callname=GetSingleItem&responseencoding=JSON&appid=${APIkey}&siteid=0&version=967&ItemID=`;
-      fetch(categoryUrl)  //first API call (getting ID of items from categories)
-        .then(response => response.json())
-        .then(categoryData => categoryData.findItemsByCategoryResponse[0].searchResult[0].item)
-        .then(result => { //second API call (getting data info from item ID)
-          const dataPromisesArray = result.map(items =>
-            fetch(`${itemUrl}${items.itemId[0]}`)
-              .then(response => response.json()));
-          return Promise.all(dataPromisesArray);
-        })
-        .then(itemData => {  //storing only what i need inside category.data
-          itemData.map((singleItem) =>
-            category.data.push({
-              name: singleItem.Item.Title,
-              imageS: singleItem.Item.GalleryURL,
-              imageL: singleItem.Item.PictureURL[0],
-              price: singleItem.Item.ConvertedCurrentPrice.Value,
-            })
-          );
-          let firstShop = category.data.slice(0, category.data.length / 3);  //splitting that data in 3 (to simulate 3 different shops)
-          let secondShop = category.data.slice(category.data.length / 3, 2 * category.data.length / 3);
-          let thirdShop = category.data.slice(2 * category.data.length / 3, category.data.length);
-            
-            if (category.id === 10968) {
-              setJ({ id: category.id, shops: [firstShop, secondShop, thirdShop] })
-            }
-            else  if (category.id === 175759) {
-              setC({ id: category.id, shops: [firstShop, secondShop, thirdShop] })
-            }
-            else {
-              setE({ id: category.id, shops: [firstShop, secondShop, thirdShop] })
-            }
-           
-        });
-    };
-/*     category.map((e) => apiCall(e));
-    setLoading(false)
-  }  */
-
- 
-    return (
-      <MyContext.Provider value={{ state, setState, loading, setLoading, itemsEbay, setItems }}>
-        {children}
-      </MyContext.Provider>
-    );
-}
-export default MyAPIProvider;
-
- /* checked 12/12/20. still working (12 items per shop) */
-   /* componentDidMount() {
     const itemsPerShop = 1; //keep this number = 1 during production. NOTE we do 3*(3n+1) API calls
     let category = [
       { id: 10968, data: [] }, //JEWELRY
       { id: 175759, data: [] }, //CLOTHES
       { id: 32852, data: [] }, //ELECTRONICS
     ];
-    let finalState = []
-    const cors = `https://cors-anywhere.herokuapp.com/`; //anti CORS <3  //USE CHROME CORS EXTENSION
+    let finalState = [
+      { id: 0, shops: [] },
+      { id: 0, shops: [] },
+      { id: 0, shops: [] },
+    ];
     const apiCall = (category) => {
-      this.setState({ loading: true })
-      const categoryUrl = `https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByCategory&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${APIkey}&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&categoryId=${category.id}&paginationInput.entriesPerPage=${itemsPerShop * 3}`;
+      setLoading(true);
+      const categoryUrl = `https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByCategory&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${APIkey}&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&categoryId=${
+        category.id
+      }&paginationInput.entriesPerPage=${itemsPerShop * 3}`;
       const itemUrl = `https://open.api.ebay.com/shopping?callname=GetSingleItem&responseencoding=JSON&appid=${APIkey}&siteid=0&version=967&ItemID=`;
-      fetch(categoryUrl)  //first API call (getting ID of items from categories)
-        .then(response => response.json())
-        .then(categoryData => categoryData.findItemsByCategoryResponse[0].searchResult[0].item)
-        .then(result => { //second API call (getting data info from item ID)
-          const dataPromisesArray = result.map(items =>
-            fetch(`${itemUrl}${items.itemId[0]}`)
-              .then(response => response.json()));
+      fetch(categoryUrl) //first API call (getting ID of items from categories)
+        .then((response) => response.json())
+        .then(
+          (categoryData) =>
+            categoryData.findItemsByCategoryResponse[0].searchResult[0].item
+        )
+        .then((result) => {
+          //second API call (getting data info from item ID)
+          const dataPromisesArray = result.map((items) =>
+            fetch(`${itemUrl}${items.itemId[0]}`).then((response) =>
+              response.json()
+            )
+          );
           return Promise.all(dataPromisesArray);
         })
-        .then(itemData => {  //storing only what i need inside category.data
+        .then((itemData) => {
+          //storing only what i need inside category.data
           itemData.map((singleItem) =>
             category.data.push({
               name: singleItem.Item.Title,
@@ -115,16 +64,58 @@ export default MyAPIProvider;
               price: singleItem.Item.ConvertedCurrentPrice.Value,
             })
           );
-          let firstShop = category.data.slice(0, category.data.length / 3);  //splitting that data in 3 (to simulate 3 different shops)
-          let secondShop = category.data.slice(category.data.length / 3, 2 * category.data.length / 3);
-          let thirdShop = category.data.slice(2 * category.data.length / 3, category.data.length);
-          finalState.push({ id: category.id, shops: [firstShop, secondShop, thirdShop] })
-          
-        });
-        
-    };
-    category.map((e) => apiCall(e))
-    this.setState({ items: finalState, loading: false })
-  } */
+          let firstShop = category.data.slice(0, category.data.length / 3); //splitting that data in 3 (to simulate 3 different shops)
+          let secondShop = category.data.slice(
+            category.data.length / 3,
+            (2 * category.data.length) / 3
+          );
+          let thirdShop = category.data.slice(
+            (2 * category.data.length) / 3,
+            category.data.length
+          );
 
-  
+          switch (category.id) {
+            case 10968:
+              finalState = [
+                { id: category.id, shops: [firstShop, secondShop, thirdShop] },
+                finalState[1],
+                finalState[2],
+              ];
+              setItems(finalState);
+              if(finalState[1].id !== 0 && finalState[2].id !== 0){setLoading(false)}
+              break;
+            case 175759:
+              finalState = [
+                finalState[0],
+                { id: category.id, shops: [firstShop, secondShop, thirdShop] },
+                finalState[2],
+              ];
+              setItems(finalState);
+              if(finalState[0].id !== 0 && finalState[2].id !== 0){setLoading(false)}
+              break;
+            case 32852:
+              finalState = [
+                finalState[0],
+                finalState[1],
+                { id: category.id, shops: [firstShop, secondShop, thirdShop] },
+              ];
+              setItems(finalState);
+              if(finalState[0].id !== 0 && finalState[1].id !== 0){setLoading(false)}
+              break;
+            default:
+              break;
+          }
+        });
+    };
+    category.map((e) => apiCall(e));
+  };
+
+  return (
+    <MyContext.Provider
+      value={{ state, setState, loading, setLoading, itemsEbay, setItems }}
+    >
+      {children}
+    </MyContext.Provider>
+  );
+};
+export default MyAPIProvider;
